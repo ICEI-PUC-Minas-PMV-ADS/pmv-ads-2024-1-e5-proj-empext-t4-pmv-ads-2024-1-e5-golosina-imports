bb07eca844ede0eafdfa62fe4428ee90d296c8d8
kellycesario <kellycesario@gmail.com>
2024-03-21 09:29:55 -0300

    feat: criação do footer

---
+++ codigo-fonte/frontend/src/atomic/components/organisms/Footer/index.stories.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "./index";
+
+export default {
+  title: "Organisms/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "padded",
+  },
+  args: {},
+  argTypes: {},
+};
+
+export const Default = {};
---
+++ codigo-fonte/frontend/src/atomic/components/organisms/Footer/index.tsx
@@ -0,0 +1,29 @@
+import { ListItem } from "@/atomic/components/atoms/ListItem";
+import { Text } from "@/atomic/components/atoms/Text";
+import styles from "./styles.module.scss";
+import navigation from "@/data/navigation.json";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Text
+        align="center"
+        children="Conectando você ao melhor em sabor e qualidade, uma mordida de cada vez"
+      />
+      <nav>
+        <ul className={styles.footer__list}>
+          {navigation.map((item, index) => (
+            <ListItem
+              key={index}
+              children={item.children}
+              href={item.href}
+              isLink={true}
+              align="center"
+              weight="400"
+            />
+          ))}
+        </ul>
+      </nav>
+    </footer>
+  );
+};
