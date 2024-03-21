e52a369caf10e7a5eda0aee39dec792830161fd9
kellycesario <kellycesario@gmail.com>
2024-03-20 23:43:10 -0300

    update: importação do main.scss no preview do storybook e criação de backgrounds globais

--- codigo-fonte/frontend/.storybook/preview.ts
+++ codigo-fonte/frontend/.storybook/preview.ts
@@ -1,15 +1,29 @@
 import type { Preview } from "@storybook/react";
+import '../src/styles/main.scss'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
+    backgrounds: {
+      default: '',
+      values: [
+        {
+          name: 'dark-mode',
+          value: '#000706',
+        },
+        {
+          name: 'light-gray',
+          value: '#f5f7fa',
+        },
+      ],
+    },
   },
-};
+}
 
 export default preview;
