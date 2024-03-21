42b5a9a0092e08623b1ea8de76dc0f514bd0c726
kellycesario <kellycesario@gmail.com>
2024-03-21 09:29:36 -0300

    feat: criação da molécula hero

---
+++ codigo-fonte/frontend/src/atomic/components/molecules/Hero/index.stories.tsx
@@ -0,0 +1,14 @@
+import { Hero } from "./index";
+
+export default {
+  title: "Molecules/Hero",
+  component: Hero,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "padded",
+  },
+  args: {},
+  argTypes: {},
+};
+
+export const Default = {};
---
+++ codigo-fonte/frontend/src/atomic/components/molecules/Hero/index.tsx
@@ -0,0 +1,40 @@
+import { Text } from "@/atomic/components/atoms/Text";
+import { Heading } from "@/atomic/components/atoms/Heading";
+import { Button } from "@/atomic/components/atoms/Button";
+import styles from "./styles.module.scss";
+
+export const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <Text
+        align="center"
+        children="Sabor argentino à sua porta!"
+      />
+      <Heading
+        align="center"
+        children="Golosinas Imports"
+        color="dark-gray"
+        level="1"
+      />
+      <Text
+        align="center"
+        children="Mergulhe na experiência única dos autênticos alfajores argentinos, onde cada mordida é uma viagem ao sabor e à tradição."
+        lineHeight="3rem"
+      />
+      <div className={styles.hero__buttons}>
+        <Button
+          level="primary"
+          label="Comprar"
+          isButton={false}
+          href="/produtos"
+        />
+        <Button
+          level="quaternary"
+          label="Fale conosco"
+          isButton={false}
+          href="/contato"
+        />
+      </div>
+    </div>
+  );
+};
