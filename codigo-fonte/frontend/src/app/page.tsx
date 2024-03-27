2976ac8ab89f108836817d9394ed9110cbc19087
kellycesario <kellycesario@gmail.com>
2024-03-27 20:54:16 -0300

    feat: importação do ArticleList na homepage

--- codigo-fonte/frontend/src/app/page.tsx
+++ codigo-fonte/frontend/src/app/page.tsx
@@ -2,12 +2,14 @@ import { BannerCTA } from "@/organisms/CTABanner";
 import styles from "./homepage.module.scss";
 import { ProductList } from "@/organisms/ProductList";
 import { Testimonials } from "@/organisms/Testimonials";
+import { ArticleList } from "@/organisms/ArticleList";
 
 export default function Home() {
   return (
     <main className={styles.homepage}>
       <ProductList />
       <Testimonials />
+      <ArticleList />
       <div className={styles.homepage__cta}>
         <BannerCTA
           cta="Peça já a sua golosina importada"
