cab1a999573090230033cf0197dde8e25b070402
kellycesario <kellycesario@gmail.com>
2024-04-21 12:37:05 -0300

    feat: criação do componente 'heading search'

---
+++ codigo-fonte/frontend/src/atomic/components/organisms/HeadingSearch/index.stories.tsx
@@ -0,0 +1,14 @@
+import { HeadingSearch } from "./index";
+
+export default {
+  title: "Organisms/Heading Search",
+  component: HeadingSearch,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  args: {},
+  argTypes: {},
+};
+
+export const Default = {};
---
+++ codigo-fonte/frontend/src/atomic/components/organisms/HeadingSearch/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { Heading } from "@/atoms/Heading";
+import { Text } from "@/atoms/Text";
+import { Search } from "@/molecules/Search";
+import styles from "./styles.module.scss";
+
+export const HeadingSearch = () => {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+
+  return (
+    <div className={styles.headingSearch}>
+      <div className={styles.headingSearch__container}>
+        <Heading align="left" children="Golosinas" color="wenge" level="2" />
+        {isHome && (
+          <Text
+            align="left"
+            children="Explore uma variedade deliciosa de alfajores artesanais e irresistíveis. Sabores únicos e textura macia em cada mordida."
+            color="wenge"
+            lineHeight="3.8rem"
+          />
+        )}
+        <Search />
+      </div>
+    </div>
+  );
+};
