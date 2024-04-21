27d468a9243a60480c89e0e365a06e69d3f61de0
jnunesss <103083123+jnunesss@users.noreply.github.com>
2024-04-21 15:34:28 -0300

    feat: limpar input comentario

--- codigo-fonte/frontend/src/atomic/components/molecules/NewComment/index.tsx
+++ codigo-fonte/frontend/src/atomic/components/molecules/NewComment/index.tsx
@@ -1,13 +1,15 @@
+"use client"
 import { Button } from "@/atoms/Button";
 import { PencilSimple } from "@phosphor-icons/react/dist/ssr";
 import styles from "./styles.module.scss";
+import { LegacyRef, MutableRefObject, useRef } from "react";
 
 interface NewCommentProps {
   text: string;
   placeholder: string;
 }
-
 export const NewComment = ({ text, placeholder }: NewCommentProps) => {
+  const commentInput = useRef<{ value: string } | undefined >({ value: "" });
   return (
     <div className={styles.comment}>
       <label htmlFor="comment" className={styles.comment__label}>
@@ -19,11 +21,13 @@ export const NewComment = ({ text, placeholder }: NewCommentProps) => {
           placeholder={placeholder}
           className={styles.comment__textarea}
           id="comment"
-/>
+          // @ts-ignore
+          ref={commentInput}
+        />
       </div>
       <div className={styles.comment__buttons}>
         <Button label="Enviar comentário" level="secondary" />
-        <Button label="Cancelar" level="quaternary" />
+        <Button label="Cancelar" level="quaternary" onClick={() => commentInput.current!.value = ""} />
       </div>
     </div>
   );
