f45acd19666b093ae453f1453a8e9c9760fd5dc9
jnunesss <103083123+jnunesss@users.noreply.github.com>
2024-05-30 21:33:23 -0300

    chore: tipos backend

--- codigo-fonte/backend/src/controllers/auth.ts
+++ codigo-fonte/backend/src/controllers/auth.ts
@@ -3,13 +3,14 @@ import UserModel from "../models/user";
 import { signJWT } from '../auth';
 import { ErrorReason, ErrorStatusCodes } from '../types/error';
 import { AppError } from '../error';
+import { AuthUserResponse } from '../types/user';
 
 interface LoginRequest {
     email: String;
     password: String;
 }
 
-const loginUser = async (request: Request, response: Response, next: NextFunction) => {
+const loginUser = async (request: Request, response: Response<AuthUserResponse>, next: NextFunction) => {
     const { email, password } = request.body as LoginRequest;
     try {
         if (!email || !password) {
@@ -22,7 +23,13 @@ const loginUser = async (request: Request, response: Response, next: NextFunctio
             throw new AppError(ErrorReason.LOGIN_FAILED, ErrorStatusCodes.BAD_REQUEST)
         }
         const token = signJWT({ userId: user.id });
-        return response.status(200).json({ token })
+        return response.status(201).json({
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }, token
+        })
 
     } catch (error) {
         next(error)
